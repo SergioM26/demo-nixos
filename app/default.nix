3f9d0cfb5bb192791a9cfcfa01e9055744a1845d
SergioM26 <sergioalejandro.moreno04@gmail.com>
2025-11-27 02:48:50 -0600

    update

--- app/default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs }:
-pkgs.python3Packages.buildPythonApplication {
-  pname = "demo-app";
-  version = builtins.readFile ./version.txt;
-  src = ./.;
-  propagatedBuildInputs = [];
-  doCheck = false;
-}
